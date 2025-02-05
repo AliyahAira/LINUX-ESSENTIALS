0a409ba29a40cc7df3fd662a4172821bd81a4639
Nekoayah <133284929+AliyahAira@users.noreply.github.com>
2025-02-05 01:41:09 +0000

    Feb 5, 2025 ONSITE

---
+++ activity1.sh
@@ -0,0 +1,20 @@
+#Declare a variable
+#USER_NAME="Student"
+#AGE= "Age"
+#PROGRAM= "Program"
+
+#Print a greeting using the variable
+#echo "Hello, $USER_NAME! Welcome to Bash scripting."
+
+#Ask for user input
+echo "Enter Name:" 
+read USER_NAME
+
+echo "Enter your Age:"
+read AGE
+
+echo "Enter your Program:"
+read PROGRAM
+
+#Display a personalized message
+echo "Hello $USER_NAME! Your age is, $AGE. and your program is $PROGRAM."
---
+++ sample.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Declare a variable
+USER_NAME="Student"
+#AGE= "Age"
+#PROGRAM= "Program"
+
+#Print a greeting using the variable
+echo "Hello, $USER_NAME! Welcome to Bash scripting."
+
+#Ask for user input
+echo "Enter your Name:" 
+read USER_NAME
+
+#echo "Enter your Age:"
+#read AGE
+
+#echo "Enter your Program:"
+#read PROGRAM
+
+#Display a personalized message
+echo "Hello, $USER_NAME!"
+
+#ILOVECODING<3!
